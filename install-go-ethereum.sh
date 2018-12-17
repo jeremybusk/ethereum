2922de9077acb37ec26016b1d42b9963745d620d
Jeremy Busk <jeremy@pyrofex.net>
2018-12-17 14:17:05 -0700

    Update install-go-ethereum.sh

--- install-go-ethereum.sh
+++ install-go-ethereum.sh
@@ -1,21 +1,26 @@
-#!/usr/bin/env bash
-## Quick & simple install using ethereum ppa.
-## Install via bash <(curl -s https://some-source-text-file)
-## Start via: systemctl start go-ethereum
-## Files are located in /var/lib/go-ethereum/.ethereum
+#!/bin/sh
+## Quick & simple install ethereum as service using ethereum ppa for Ubuntu.
+# bash <(curl -s https://raw.githubusercontent.com/jeremybusk/ethereum/master/install-go-ethereum.sh)
+# systemctl start go-ethereum
+# /var/lib/go-ethereum/.ethereum
 
-
-set -exo pipefail
+set -e
 
 USERNAME="go-ethereum"
 
+ID=$(cat /etc/*release | grep '^DISTRIB_CODENAME=' | awk -F= '{print $2}')
+if [ "${ID}" = "bionic" ] || [ "${ID}" "buster" ] ; then
+    echo "Supported host operating system Ubuntu Bionic/Buster 16.04/18.04."
+else
+    echo "ERROR! Invalid host operating system."
+    exit 1
+fi
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y build-essential git golang
-git clone https://github.com/ethereum/go-ethereum
-cd go-ethereum
-make geth
-cp build/bin/geth /usr/bin/
+sudo apt-get install -y ethereum
 
 cat > /etc/systemd/system/${USERNAME}.service <<EOF
 [Unit]
@@ -25,7 +30,7 @@ After=network.target
 [Service]
 User=${USERNAME}
 WorkingDirectory=/var/lib/${USERNAME}
-ExecStart=/usr/bin/geth --ws --rpc --rpcaddr 0.0.0.0 --rpcport 8545 
+ExecStart=/usr/bin/go-ethereum --ws --rpc --rpcaddr 0.0.0.0 --rpcport 8545 
 Restart=on-failure
 
 [Install]
@@ -33,8 +38,9 @@ WantedBy=multi-user.target
 EOF
 
 mkdir -p /var/lib/${USERNAME}
-groupadd ${USERNAME} 
-useradd --gid ${USERNAME} --home /var/lib/${USERNAME} ${USERNAME} 
-# cp -rp /root/${USERNAME} /var/lib/${USERNAME}
-chown -R ${USERNAME}:${USERNAME} /var/lib/${USERNAME}
+useradd --home /var/lib/${USERNAME} ${USERNAME}
+chown ${USERNAME}:${USERNAME} /var/lib/${USERNAME}
+
 systemctl enable ${USERNAME}.service
+
+exit 0
